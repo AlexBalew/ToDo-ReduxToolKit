bd8bf8733f11e803005cb5f417fde6f8c862476c
AlexBalew <balew48@gmail.com>
2021-12-05 20:04:16 +0300

    store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,9 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {combineReducers} from "redux";
 import {todolistsReducer} from "../Reducers/todolist.reducer";
 import {tasksReducer} from "../Reducers/tasks.reducer";
 import {appReducer} from "../Reducers/app-reducer";
 import thunk from "redux-thunk";
 import {authReducer} from "../Reducers/authReducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 export type MainReducerType = ReturnType<typeof mainReducer>
 
@@ -14,7 +15,12 @@ let mainReducer = combineReducers({
     login: authReducer,
 })
 
-export let store = createStore(mainReducer, applyMiddleware(thunk))
+//export let store = createStore(mainReducer, applyMiddleware(thunk)) //redux approach
+
+export const store = configureStore({ //redux toolkit approach
+    reducer: mainReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk)
+})
 
 // @ts-ignore
 window.store = store
